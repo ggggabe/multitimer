76b4d1aa5e634094acaa5a852f36d54afd8759d5
ggggabe <who@gabe.is>
2020-11-23 17:28:19 -0500

    upgrade editable component with a done button

--- src/components/index.js
+++ src/components/index.js
@@ -53,7 +53,6 @@ export const InputGroup = (props) => {
 
 
 const EXIT_EDIT_KEY_CODES = [13, 27]
-
 // PROP TYPE INFO
 //
 // props = {
@@ -78,14 +77,15 @@ export const Editable = (props) => {
 
   const [editable, setEditable] = useState(false)
   const [editedValue, setCurrentEditedValue] = useState(value)
-
-  const exit = useMemo(() => e => {
-    if (!EXIT_EDIT_KEY_CODES.filter( k => k === e.keyCode ).length ) return
-
-    e.preventDefault()
+  const exit = () => {
     onEdit(editedValue)
     setEditable(false)
+  }
 
+  const handleExitHotKeys = useMemo(() => e => {
+    if (!EXIT_EDIT_KEY_CODES.filter( k => k === e.keyCode ).length ) return
+    e.preventDefault()
+    exit()
   }, [onEdit, editedValue])
 
   useEffect(() => {
@@ -93,12 +93,12 @@ export const Editable = (props) => {
 
     const inputElement = ref.current
 
-    inputElement && inputElement.addEventListener('keypress', exit)
+    inputElement && inputElement.addEventListener('keypress', handleExitHotKeys)
 
     return () => {
-      inputElement && inputElement.removeEventListener('keypress', exit)
+      inputElement && inputElement.removeEventListener('keypress', handleExitHotKeys)
     }
-  }, [editable, exit, ref])
+  }, [editable, handleExitHotKeys, ref])
 
   return (
     <div style={{ cursor: 'pointer' }} onClick={() => editable || setEditable(true)}>
@@ -110,6 +110,9 @@ export const Editable = (props) => {
               e.preventDefault()
               setCurrentEditedValue(e.target.value)
             }} />
+            <Button onClick={() => { exit() } }>
+              <code>Done</code>
+            </Button>
           </InputGroup>
         )
         : children
